a7b04a0bd20dfe9356c5b7dcafd43e3de43143c0
Evelyn Fischer <e.fischer@gmx.com>
2022-06-26 18:42:49 +0200

    Add new empty activity for account creation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FinanceTracker
 
+FinanceTracker is an open source personal finance tracking application for android devices. 
 
-
-# Icon References
+## Icon References
 
 * [App Logo by Aranagraphics](https://www.flaticon.com/de/premium-icon/mobiles-banking_3956555?term=mobile%20banking&page=1&position=16&page=1&position=16&related_id=3956555&origin=tag)
--- app/src/main/java/de/efischer/financetracker/activities/MainActivity.java
+++ app/src/main/java/de/efischer/financetracker/activities/MainActivity.java
@@ -2,6 +2,7 @@ package de.efischer.financetracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,14 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            getSupportActionBar().hide();
-        } catch(NullPointerException e) {
-            e.printStackTrace();
-        }
     }
 
     public void addAccount(View view) {
-
+        Intent intent = new Intent(this, AddAccountActivity.class);
+        startActivity(intent);
     }
 }
