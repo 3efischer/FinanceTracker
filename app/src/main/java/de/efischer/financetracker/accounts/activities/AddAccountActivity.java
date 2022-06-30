066b4add6cad1a6454cf5526cbb22977d01ce3eb
Evelyn Fischer <evelyn.fischer91@hotmail.com>
2022-07-01 01:44:25 +0200

    Created account type and credit card type dropdowns in add account activity

--- app/src/main/java/de/efischer/financetracker/accounts/activities/AddAccountActivity.java
+++ app/src/main/java/de/efischer/financetracker/accounts/activities/AddAccountActivity.java
@@ -1,6 +1,8 @@
 package de.efischer.financetracker.accounts.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import de.efischer.financetracker.R;
 import de.efischer.financetracker.accounts.fragments.AccountDropdownAdapter;
 import de.efischer.financetracker.accounts.model.valueobjects.AccountType;
+import de.efischer.financetracker.accounts.model.valueobjects.CreditCardType;
 import de.efischer.financetracker.common.inputs.TextInputFragment;
 
-public class AddAccountActivity extends AppCompatActivity {
+public class AddAccountActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,21 @@ public class AddAccountActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             setupAccountDropdown();
+            setupCreditCardDropdown();
             setupNameField();
+
         }
+    }
+
+    private void setupNameField() {
+        Bundle args = new Bundle();
+        args.putInt(TextInputFragment.DESCRIPTION_TEXT_KEY, R.string.account_name);
+        args.putInt(TextInputFragment.TEXTFIELD_HINT_KEY, R.string.account_name);
+
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.account_name_input_fragment, TextInputFragment.class, args)
+                .commit();
     }
 
     private void setupAccountDropdown() {
@@ -35,17 +51,33 @@ public class AddAccountActivity extends AppCompatActivity {
 
         Spinner spinner = findViewById(R.id.account_type_dropdown);
         spinner.setAdapter(new AccountDropdownAdapter(this, R.layout.account_type_dropdown_item, accountTypesAsStrings, imageIdArray));
-
+        spinner.setOnItemSelectedListener(this);
     }
 
-    private void setupNameField() {
-        Bundle args = new Bundle();
-        args.putInt(TextInputFragment.DESCRIPTION_TEXT_KEY, R.string.account_name);
-        args.putInt(TextInputFragment.TEXTFIELD_HINT_KEY, R.string.account_name);
+    private void setupCreditCardDropdown() {
+        CreditCardType[] creditCardTypes = CreditCardType.values();
+        String[] creditCardTypesAsStrings = new String[creditCardTypes.length];
+        Integer[] imageIdArray = new Integer[creditCardTypes.length];
 
-        getSupportFragmentManager().beginTransaction()
-                .setReorderingAllowed(true)
-                .add(R.id.account_name_input_fragment, TextInputFragment.class, args)
-                .commit();
+        for(int i = 0; i < creditCardTypes.length; i++) {
+            creditCardTypesAsStrings[i] = getResources().getString(creditCardTypes[i].nameId);
+            imageIdArray[i] = creditCardTypes[i].iconId;
+        }
+
+        Spinner spinner = findViewById(R.id.credit_card_type_dropdown);
+        spinner.setAdapter(new AccountDropdownAdapter(this, R.layout.account_type_dropdown_item, creditCardTypesAsStrings, imageIdArray));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            AccountType[] accountTypes = AccountType.values();
+
+            Spinner creditCardTypeSpinner = findViewById(R.id.credit_card_type_dropdown);
+            creditCardTypeSpinner.setVisibility(accountTypes[position] == AccountType.CREDIT_CARD ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/de/efischer/financetracker/accounts/fragments/AccountOverviewFragment.java
+++ app/src/main/java/de/efischer/financetracker/accounts/fragments/AccountOverviewFragment.java
@@ -5,17 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
 import de.efischer.financetracker.R;
 import de.efischer.financetracker.accounts.model.entities.Account;
+import de.efischer.financetracker.accounts.model.valueobjects.AccountType;
+import de.efischer.financetracker.accounts.model.valueobjects.Amount;
 
 /**
  * Fragment that represents a list of all accounts.
@@ -46,9 +49,7 @@ public class AccountOverviewFragment extends Fragment {
         recyclerView.setAdapter(adapter);
 
         // Set divider between items
-        DividerItemDecoration divider = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-        divider.setDrawable(ContextCompat.getDrawable(recyclerView.getContext(), R.drawable.horizontal_divider));
-        recyclerView.addItemDecoration(divider);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
 
         // Create touch helper for drag and drop and attach to recyclerview
         ItemTouchHelper.Callback callback = new AccountItemMovementTouchHelper(adapter);
@@ -89,10 +90,10 @@ public class AccountOverviewFragment extends Fragment {
     private void initDataset() {
 
         this.accounts = new ArrayList<>();
-        //this.accounts.add(new Account("Bargeld", AccountType.CASH, Amount.of(new BigDecimal(25), Currency.getInstance("EUR"))));
-//        this.accounts.add(new Account("Girokonto", AccountType.BANK, Amount.of(new BigDecimal(2000), Currency.getInstance("EUR"))));
-//        this.accounts.add(new Account("Sparbuch", AccountType.SAVINGS, Amount.of(new BigDecimal(-125), Currency.getInstance("EUR"))));
-//        this.accounts.add(new Account("Kreditkarte", AccountType.CREDIT_CARD, Amount.of(new BigDecimal(5000), Currency.getInstance("EUR"))));
+        this.accounts.add(new Account("Bargeld", AccountType.CASH, Amount.of(new BigDecimal(25), Currency.getInstance("EUR"))));
+        this.accounts.add(new Account("Girokonto", AccountType.BANK, Amount.of(new BigDecimal(2000), Currency.getInstance("EUR"))));
+        this.accounts.add(new Account("Sparbuch", AccountType.SAVINGS, Amount.of(new BigDecimal(-125), Currency.getInstance("EUR"))));
+        this.accounts.add(new Account("Kreditkarte", AccountType.CREDIT_CARD, Amount.of(new BigDecimal(5000), Currency.getInstance("EUR"))));
     }
 }
 
