2088b9b521702803b52fa7e508eb6ac7cf9efee6
Evelyn Fischer <evelyn.fischer91@hotmail.com>
2022-07-02 04:24:02 +0200

    Focus changes from integral to decimal when pressing dot or comma

--- app/src/main/java/de/efischer/financetracker/common/inputs/AmountInputFragment.java
+++ app/src/main/java/de/efischer/financetracker/common/inputs/AmountInputFragment.java
@@ -1,11 +1,14 @@
 package de.efischer.financetracker.common.inputs;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,8 +24,6 @@ public class AmountInputFragment extends Fragment {
     public static final String IS_STARTING_POSITIVE = "IS_STARTING_POSITIVE";
     public static final String AMOUNT_TYPE_TITLE = "AMOUNT_TYPE_TITLE";
 
-    private boolean isPositive;
-    private int titleId;
     private int integral;
     private int decimal;
     private BigInteger amount;
@@ -34,24 +35,48 @@ public class AmountInputFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        this.isPositive = requireArguments().getBoolean(IS_STARTING_POSITIVE);
-        this.titleId = requireArguments().getInt(AMOUNT_TYPE_TITLE);
-        return inflater.inflate(R.layout.fragment_amount_input, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_amount_input, container, false);
+
+        int titleId = requireArguments().getInt(AMOUNT_TYPE_TITLE);
+        TextView amountTitle = view.findViewById(R.id.amount_input_title);
+        amountTitle.setText(titleId);
+
+        boolean isPositive = requireArguments().getBoolean(IS_STARTING_POSITIVE);
+        ToggleButton toggleButton = view.findViewById(R.id.toggleImage);
+        toggleButton.setChecked(isPositive);
+        toggleButton.setBackgroundResource(isPositive ? R.drawable.ic_plus : R.drawable.ic_minus);
+
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TextView inputFieldTitle = view.findViewById(R.id.amount_input_type);
-        inputFieldTitle.setText(titleId);
+        ToggleButton toggleButton = view.findViewById(R.id.toggleImage);
+        toggleButton.setOnClickListener(v ->
+                toggleButton.setBackgroundResource(toggleButton.isChecked() ? R.drawable.ic_plus : R.drawable.ic_minus));
 
-        ImageView signToggleImage = view.findViewById(R.id.toggleImage);
-        signToggleImage.setBackgroundResource(this.isPositive ? R.drawable.ic_plus : R.drawable.ic_minus);
+        EditText integralTextField = view.findViewById(R.id.integralPartInput);
+        EditText decimalTextField = view.findViewById(R.id.decimalPartInput);
+        integralTextField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        signToggleImage.setOnClickListener(v -> {
-            isPositive = !isPositive;
-            signToggleImage.setBackgroundResource(isPositive ? R.drawable.ic_plus : R.drawable.ic_minus);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.charAt(start) == '.' || s.charAt(start) == ',') {
+                    integralTextField.setText(s.subSequence(0, start));
+                    decimalTextField.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
         });
     }
 }
