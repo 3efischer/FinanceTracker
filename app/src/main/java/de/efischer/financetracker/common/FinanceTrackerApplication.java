15418f2c4442b63f776d7d21052f0ae2c69936c3
Evelyn Fischer <evelyn.fischer91@hotmail.com>
2022-07-16 17:09:47 +0200

    Added abort button

---
+++ app/src/main/java/de/efischer/financetracker/common/FinanceTrackerApplication.java
@@ -0,0 +1,7 @@
+package de.efischer.financetracker.common;
+
+import android.app.Application;
+
+public class FinanceTrackerApplication extends Application {
+
+}
---
+++ app/src/main/java/de/efischer/financetracker/common/topbar/TopBarSelectedOptionViewModel.java
@@ -0,0 +1,8 @@
+package de.efischer.financetracker.common.topbar;
+
+import androidx.lifecycle.ViewModel;
+
+public class TopBarSelectedOptionViewModel extends ViewModel {
+
+    
+}
