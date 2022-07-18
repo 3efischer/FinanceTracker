6c97587fce39db9fce6add39d14f158e6a3f71a5
Evelyn Fischer <evelyn.fischer91@hotmail.com>
2022-07-18 20:39:56 +0200

    Added binding in AccountOverviewFragment

--- app/src/main/java/de/efischer/financetracker/accounts/fragments/overview/AccountOverviewFragment.java
+++ app/src/main/java/de/efischer/financetracker/accounts/fragments/overview/AccountOverviewFragment.java
@@ -5,60 +5,62 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Currency;
 import java.util.List;
+import java.util.Objects;
 
-import de.efischer.financetracker.R;
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
 import de.efischer.financetracker.accounts.model.entities.Account;
-import de.efischer.financetracker.accounts.model.valueobjects.AccountType;
-import de.efischer.financetracker.accounts.model.valueobjects.Amount;
+import de.efischer.financetracker.common.ApplicationDatabase;
+import de.efischer.financetracker.databinding.FragmentAccountOverviewBinding;
 
 /**
  * Fragment that represents a list of all accounts.
+ *
  * @author Evelyn Fischer
  */
 
+@AndroidEntryPoint
 public class AccountOverviewFragment extends Fragment {
 
+    @Inject
+    ApplicationDatabase database;
+
+    private FragmentAccountOverviewBinding binding;
     private RecyclerView recyclerView;
     private List<Account> accounts;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initDataset();
+        accounts = Objects.requireNonNullElseGet(database.accountDao().getAll().getValue(), ArrayList::new);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_account_overview, container, false);
+        this.binding = FragmentAccountOverviewBinding.inflate(inflater, container, false);
 
-        // Set layout for recycler view
-        recyclerView = view.findViewById(R.id.recyclerView);
-
-        // Create and attach list item adapter to recycler view
         AccountListItemAdapter adapter = new AccountListItemAdapter(accounts);
-        recyclerView.setAdapter(adapter);
-
-        // Set divider between items
-        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.addItemDecoration(new DividerItemDecoration(binding.recyclerView.getContext(), DividerItemDecoration.VERTICAL));
 
         // Create touch helper for drag and drop and attach to recyclerview
         ItemTouchHelper.Callback callback = new AccountItemMovementTouchHelper(adapter);
         ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
-        touchHelper.attachToRecyclerView(recyclerView);
+        touchHelper.attachToRecyclerView(binding.recyclerView);
 
         // Handle changes on recyclerview
-        if(accounts.isEmpty()) {
-            recyclerView.setVisibility(View.GONE);
+        if (accounts.isEmpty()) {
+            binding.recyclerView.setVisibility(View.GONE);
         }
         adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
@@ -80,20 +82,11 @@ public class AccountOverviewFragment extends Fragment {
             }
 
             public void checkIfEmpty() {
-                recyclerView.setVisibility(adapter.getItemCount() > 0 ? View.VISIBLE : View.GONE);
+                binding.recyclerView.setVisibility(adapter.getItemCount() > 0 ? View.VISIBLE : View.GONE);
             }
         });
 
-        return view;
-    }
-
-    private void initDataset() {
-
-        this.accounts = new ArrayList<>();
-        this.accounts.add(new Account("Bargeld", AccountType.CASH, Amount.of(new BigDecimal(25), Currency.getInstance("EUR"))));
-        this.accounts.add(new Account("Girokonto", AccountType.BANK, Amount.of(new BigDecimal(2000), Currency.getInstance("EUR"))));
-        this.accounts.add(new Account("Sparbuch", AccountType.SAVINGS, Amount.of(new BigDecimal(-125), Currency.getInstance("EUR"))));
-        this.accounts.add(new Account("Kreditkarte", AccountType.CREDIT_CARD, Amount.of(new BigDecimal(5000), Currency.getInstance("EUR"))));
+        return binding.getRoot();
     }
 }
 
