b311112d054e0e72d153f3d8c628f13ce9cf9475
Evelyn Fischer <evelyn.fischer91@hotmail.com>
2022-07-26 23:47:12 +0200

    Added option menu for account items

--- app/src/main/java/de/efischer/financetracker/accounts/ui/list/AccountListAdapter.java
+++ app/src/main/java/de/efischer/financetracker/accounts/ui/list/AccountListAdapter.java
@@ -1,16 +1,12 @@
 package de.efischer.financetracker.accounts.ui.list;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -19,7 +15,7 @@ import java.util.List;
 import de.efischer.financetracker.R;
 import de.efischer.financetracker.accounts.model.entities.Account;
 
-public class AccountListAdapter extends ListAdapter<Account, AccountListAdapter.ViewHolder> {
+public class AccountListAdapter extends ListAdapter<Account, AccountListItemViewHolder> {
 
     private ArrayList<Account> tempAccountList;
 
@@ -29,15 +25,15 @@ public class AccountListAdapter extends ListAdapter<Account, AccountListAdapter.
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public AccountListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.account_list_item, parent, false);
 
-        return new AccountListAdapter.ViewHolder(v);
+        return new AccountListItemViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull AccountListItemViewHolder holder, int position) {
         Account account = getCurrentList().get(position);
 
         holder.getIcon().setBackgroundResource(account.getType().iconId);
@@ -55,7 +51,7 @@ public class AccountListAdapter extends ListAdapter<Account, AccountListAdapter.
         holder.getLastChangedDate().setText(account.getLastDayChanged());
         holder.getAccountName().setText(account.getName());
     }
-
+    
     public static final DiffUtil.ItemCallback<Account> accountItemCallback = new DiffUtil.ItemCallback<>() {
         @Override
         public boolean areItemsTheSame(@NonNull Account oldItem, @NonNull Account newItem) {
@@ -83,47 +79,5 @@ public class AccountListAdapter extends ListAdapter<Account, AccountListAdapter.
     public List<Account> getDragEndedResultList() {
         tempAccountList.forEach(account -> account.setListPosition(tempAccountList.indexOf(account)));
         return tempAccountList;
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        private final TextView firstLine;
-        private final TextView accountName;
-        private final TextView lastChangedDate;
-        private final TextView amount;
-        private final ImageView icon;
-        private final String TAG = ViewHolder.class.getSimpleName();
-
-        public ViewHolder(View v) {
-            super(v);
-            // Define click listener for the ViewHolder's View.
-            v.setOnClickListener(v1 -> Log.d(TAG, "Element " + getAdapterPosition() + " clicked."));
-
-            firstLine = v.findViewById(R.id.first_row_text);
-            accountName = v.findViewById(R.id.account_title);
-            lastChangedDate = v.findViewById(R.id.last_account_changed_date);
-            amount = v.findViewById(R.id.total_amount);
-            icon = v.findViewById(R.id.account_icon);
-        }
-
-        public TextView getFirstLine() {
-            return firstLine;
-        }
-
-        public TextView getAccountName() {
-            return accountName;
-        }
-
-        public TextView getLastChangedDate() {
-            return lastChangedDate;
-        }
-
-        public TextView getAmount() {
-            return amount;
-        }
-
-        public ImageView getIcon() {
-            return icon;
-        }
     }
 }
---
+++ app/src/main/java/de/efischer/financetracker/accounts/ui/list/AccountListItemViewHolder.java
@@ -0,0 +1,74 @@
+package de.efischer.financetracker.accounts.ui.list;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.MenuRes;
+import androidx.recyclerview.widget.RecyclerView;
+
+import de.efischer.financetracker.R;
+
+public class AccountListItemViewHolder extends RecyclerView.ViewHolder {
+
+    private final TextView firstLine;
+    private final TextView accountName;
+    private final TextView lastChangedDate;
+    private final TextView amount;
+    private final ImageView icon;
+    private final String TAG = AccountListItemViewHolder.class.getSimpleName();
+
+    public AccountListItemViewHolder(View v) {
+        super(v);
+
+        firstLine = v.findViewById(R.id.first_row_text);
+        accountName = v.findViewById(R.id.account_title);
+        lastChangedDate = v.findViewById(R.id.last_account_changed_date);
+        amount = v.findViewById(R.id.total_amount);
+        icon = v.findViewById(R.id.account_icon);
+
+        ImageButton menuButton = v.findViewById(R.id.menu_button);
+        menuButton.setOnClickListener(btnView -> showMenu(btnView, R.menu.account_popup_menu));
+    }
+
+    private void showMenu(View v, @MenuRes int menuRes) {
+        PopupMenu popup = new PopupMenu(v.getContext(), v);
+        popup.getMenuInflater().inflate(menuRes, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.option_edit) {
+                Log.println(Log.ERROR, null, "edit pressed");
+            } else {
+                Log.println(Log.ERROR, null, "delete pressed");
+            }
+            return true;
+        });
+
+        popup.show();
+    }
+
+    public TextView getFirstLine() {
+        return firstLine;
+    }
+
+    public TextView getAccountName() {
+        return accountName;
+    }
+
+    public TextView getLastChangedDate() {
+        return lastChangedDate;
+    }
+
+    public TextView getAmount() {
+        return amount;
+    }
+
+    public ImageView getIcon() {
+        return icon;
+    }
+
+
+}
